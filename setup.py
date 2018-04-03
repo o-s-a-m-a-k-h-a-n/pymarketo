f79824d492290c3e2e4b87bc5468fdb467ea7118
Osama Khan <mail@osamakhan.com>
2018-04-03 13:27:58 -0700

    setup pypi project

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
+from setuptools import setup, find_packages
+
 import os
 import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+
 
 # Don't import analytics-python module here, since deps may not be installed
 # sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'pymarketo'))
@@ -18,17 +17,24 @@ PyMarketo is a Python client that wraps the MarketoRestPython project to provide
 
 setup(
     name='pymarketo',
-    version= '0.0.1',
+    version= '0.0.1dev1',
+    description='Python Client for the Marketo REST API',
+    long_description=long_description,
     url='https://github.com/osamakhn/pymarketo',
     author='Osama Khan',
     author_email='osamakhn@gmail.com',
-    packages=['pymarketo'],
     license='MIT License',
     install_requires=[
         'requests',
         'marketorestpython'
     ],
-    keywords = ['Marketo', 'REST API', 'Wrapper', 'Client'],
-    description='Python Client for the Marketo REST API',
-    long_description=long_description
+    keywords = 'Marketo REST API Wrapper Client',
+    packages=find_packages(exclude=['contrib','docs','tests']),
+    classifier=['Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7'],
+    project_urls = {
+        'Source': 'https://www.github.com/osamakhn/pymarketo'
+    }
 )
